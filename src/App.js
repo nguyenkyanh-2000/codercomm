da23804067c5e3d3a99138ae58bd5f5af275c3b9
Anh Ky Nguyen <anh163@gmail.com>
2023-02-20 12:52:20 +0700

    Refactoring

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
 import ThemeProvider from "./theme";
-import {
-  BASE_URL,
-  CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_UPLOAD_PRESET,
-} from "./app/config";
-
 function App() {
   return (
     <AuthProvider>

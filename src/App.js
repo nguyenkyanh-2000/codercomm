7176b62619bcf48658d75de60278bde1e2cbe888
Anh Ky Nguyen <anh163@gmail.com>
2023-02-20 12:44:24 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
 import ThemeProvider from "./theme";
+import {
+  BASE_URL,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_UPLOAD_PRESET,
+} from "./app/config";
 
 function App() {
+  console.log(BASE_URL);
+  console.log(CLOUDINARY_CLOUD_NAME);
+  console.log(CLOUDINARY_UPLOAD_PRESET);
   return (
     <AuthProvider>
       <BrowserRouter>
